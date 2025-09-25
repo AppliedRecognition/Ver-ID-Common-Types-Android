5ddd1e18fedcf4b63b48e70589b06fb070a8e2da
Jakub Dolejs <jakubdolejs@gmail.com>
2025-09-25 14:13:47 +0100

    Use IImage interface instead of concrete Image type in facial attribute detection interfaces

--- lib/src/main/java/com/appliedrec/verid3/common/AnyFacialAttributeDetector.kt
+++ lib/src/main/java/com/appliedrec/verid3/common/AnyFacialAttributeDetector.kt
@@ -1,7 +1,7 @@
 package com.appliedrec.verid3.common
 
 class AnyFacialAttributeDetector(
-    private val detectFunc: suspend (Face, Image) -> AnyFacialAttributeDetectionResult?,
+    private val detectFunc: suspend (Face, IImage) -> AnyFacialAttributeDetectionResult?,
     private val getThreshold: () -> Float,
     private val setThreshold: (Float) -> Unit
 ) {
@@ -9,7 +9,7 @@ class AnyFacialAttributeDetector(
         get() = getThreshold()
         set(value) = setThreshold(value)
 
-    suspend fun detect(face: Face, image: Image): AnyFacialAttributeDetectionResult? {
+    suspend fun detect(face: Face, image: IImage): AnyFacialAttributeDetectionResult? {
         return detectFunc(face, image)
     }
 }
